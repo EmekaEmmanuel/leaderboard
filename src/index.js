8f71414630b1a3c1d90eb7a5437484f74c1b83f9
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-01-31 13:36:49 -0800

    Change loadHtml to  an arrow function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import displayHtml from './pageUI.js';
 
 const appContainer = document.querySelector('.app_container');
 
-function loadHtml() {
+const loadHtml = () => {
   appContainer.innerHTML = displayHtml();
-}
+};
 
 loadHtml();
